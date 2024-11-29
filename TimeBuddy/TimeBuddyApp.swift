ed7dd3397f070a545a786cbca17f66a27d4dcc6e
Weerawut <weerawut@live.com>
2024-11-29 13:00:55 +0700

    Initial Commit

---
+++ TimeBuddy/TimeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBuddyApp.swift
+//  TimeBuddy
+//
+//  Created by Weerawut Chaiyasomboon on 29/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TimeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
